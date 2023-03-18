a83a3afc31a6a30aa868f86acd3fba453b09c8b9
carlynorama <carlynorama@gmail.com>
2023-03-18 09:39:39 -0700

    Initial Commit

---
+++ CInclude/CIncludeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIncludeApp.swift
+//  CInclude
+//
+//  Created by Labtanza on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CIncludeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
